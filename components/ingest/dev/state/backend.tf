5001e4b67051750d678fdba185db4e3cc98520a5
Ajinkya Chawan <ajinkya.chawan@gmail.com>
2025-09-23 17:14:28 -0400

    created folder structure

---
+++ components/ingest/dev/state/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "SharedResources" # Replace with your resource group name
+    storage_account_name = "tfstatefiledevops"       # Replace with your storage account name
+    container_name       = "tfstate"            # Replace with your blob container name
+    key                  = "terraform.tfstate.sandbox"  # The name of your state file within the container
+  }
+}
