bc3714c1739ce3fe8ae31485bbfacb8a43a98925
Ajinkya Chawan <ajinkya.chawan@gmail.com>
2025-09-23 23:17:14 -0400

    last update

--- components/ingest/dev/main.tf
+++ components/ingest/dev/main.tf
@@ -1,5 +1,5 @@
 module "resource_group_module" {
-    source = "../../../Terraform/modules/common/resource_group"
+    source = "../../../../Terraform/modules/common/resource_group"
     resource_group_name = var.resource_group_name
     resource_group_location = var.resource_group_location
 }
