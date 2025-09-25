405400edf754835913e674d95f19581332aa50dc
Ajinkya Chawan <ajinkya.chawan@gmail.com>
2025-09-25 16:26:45 -0400

    trying with remote module

--- components/ingest/dev/main.tf
+++ components/ingest/dev/main.tf
@@ -1,21 +1,5 @@
-#module "resource_group_module" {
-#    source = "../../../../Terraform/modules/common/resource_group"
-#    resource_group_name = var.resource_group_name
-#    resource_group_location = var.resource_group_location
-#}
-
-# Create an Azure Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-  tags = {
-    Environment = "Development"
-    Project     = "TerraformDemo"
-  }
-}
-
-# Output the name of the created Resource Group
-output "resource_group_name_output" {
-  description = "The name of the created Azure Resource Group"
-  value       = azurerm_resource_group.example.name
+module "resource_group_module" {
+    source = "../../../../Terraform/modules/common/resource_group"
+    resource_group_name = var.resource_group_name
+    resource_group_location = var.resource_group_location
 }
