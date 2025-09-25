8c5e9e07526139c8d5c729679b727464168dcaf3
Ajinkya Chawan <ajinkya.chawan@gmail.com>
2025-09-25 16:10:34 -0400

    moved files

--- components/ingest/dev/tfstate/backend.tf
+++ components/ingest/dev/tfstate/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "SharedResources" # Replace with your resource group name
     storage_account_name = "tfstatefiledevops"       # Replace with your storage account name
     container_name       = "tfstate"            # Replace with your blob container name
-    key                  = "terraform.tfstate.dev"  # The name of your state file within the container
+    key                  = "terraform.tfstate.ingest.dev"  # The name of your state file within the container
   }
 }
