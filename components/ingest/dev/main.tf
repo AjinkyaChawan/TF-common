d9769dac69a932c66db70883cb75b1367bb11b6c
Ajinkya Chawan <ajinkya.chawan@gmail.com>
2025-09-26 13:46:27 -0400

    updated again

--- components/ingest/dev/main.tf
+++ components/ingest/dev/main.tf
@@ -4,7 +4,6 @@ module "resource_group_module" {
     resource_group_location = var.resource_group_location
 
     failovers = {
-            source = "../../../../Terraform/modules/common/resource_group"
             resource_group_name = var.failover_resource_group_name
             resource_group_location = var.failover_resource_group_location
     } 
